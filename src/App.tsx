ef6b327ba31e46685dd80729be49807f13007e61
Alejandro de Frutos <adefrutoscasado@gmail.com>
2020-05-08 16:33:29 +0200

    Fiddler ruleset generator first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, ChangeEvent } from 'react'
+import './App.css'
+import exportZip from 'fiddler-ruleset-generator'
+
+const initDownload = (function () {
+  let a = document.createElement("a")
+  document.body.appendChild(a)
+  // @ts-ignore
+  a.style = "display: none"
+  return function (data: any, fileName: string) {
+    a.href = "data:application/zip;base64," + data; //Image Base64 Goes here
+    a.download = "generated.zip"; //File name Here
+    a.click(); //Downloaded file
+  }
+}())
+
+interface InputFileJsonProps {
+  onChangeJson?: (json: object) => void
+  onStartProcessing?: (...args: any) => void
+}
+
+const InputFileJson = ({
+  onChangeJson = () => { },
+  onStartProcessing = () => { }
+}: InputFileJsonProps) => {
+
+  function onChangeFile(event: ChangeEvent<HTMLInputElement>) {
+    if (event.target?.files?.[0]) {
+      const reader = new FileReader()
+      reader.onload = onReaderLoad
+      reader.readAsText(event.target.files[0])
+    }
+  }
+
+  const onReaderLoad: FileReader['onload'] = (event) => {
+    if (typeof event?.target?.result === 'string') {
+      const data = JSON.parse(event.target.result)
+      onChangeJson(data as object)
+    }
+  }
+
+  return (
+    <input onChange={onChangeFile} type="file"></input>
+  )
+}
 
 function App() {
+  const [exportedZip, setExportedZip] = useState(null as null | any)
+
+  const onChangeJson = async (data: object) => {
+    const process = async () => {
+      const zip = await exportZip(data, undefined, {type: 'base64'})
+      setExportedZip(zip)
+    }
+    process()
+  }
+
+  const onClickDownload = () => {
+    initDownload(exportedZip, 'generated.zip')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <InputFileJson onChangeJson={onChangeJson} />
+        {exportedZip && <button onClick={onClickDownload}>Download</button>}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
