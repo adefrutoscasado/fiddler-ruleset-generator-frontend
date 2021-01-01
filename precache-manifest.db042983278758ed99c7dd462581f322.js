ac4e66a06c64574d1724dd21346c361d59813034
Alejandro de Frutos <adefrutoscasado@gmail.com>
2021-01-01 20:49:46 +0100

    Updates

---
+++ precache-manifest.db042983278758ed99c7dd462581f322.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c835a95f593356b2e8901e40bc4baff6",
+    "url": "/fiddler-ruleset-generator/index.html"
+  },
+  {
+    "revision": "5b8951354681a13aff8f",
+    "url": "/fiddler-ruleset-generator/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "5688d491bdf3d9d8ed96",
+    "url": "/fiddler-ruleset-generator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5b8951354681a13aff8f",
+    "url": "/fiddler-ruleset-generator/static/js/2.1e80424c.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/fiddler-ruleset-generator/static/js/2.1e80424c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5688d491bdf3d9d8ed96",
+    "url": "/fiddler-ruleset-generator/static/js/main.546a9649.chunk.js"
+  },
+  {
+    "revision": "b34c66fddeabc1595a7a",
+    "url": "/fiddler-ruleset-generator/static/js/runtime-main.a68aa943.js"
+  }
+]);
